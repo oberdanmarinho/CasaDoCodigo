05292b7b90739c6d1c414efc00bedd632805fccb
Oberdan Marinho <oberdanmarinho@gmail.com>
2021-10-01 23:51:31 -0300

    Primeiro Commit

---
+++ Aulas/Aula1/CasaDoCodigo/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using CasaDoCodigo.Models;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Sua página de contato.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
